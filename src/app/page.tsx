e1cf037e87430f69e7f0db4b7d532976bb681463
Jobwafula <jobwafulabg@gmail.com>
2024-08-15 12:27:37 +0300

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Header from "@/components/header/Header";
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <div>
     <Header />
+    <Hero />
+    </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,10 @@
 const Hero = ()=>{
-    return()
+    return(
+        <div className='flex md:flex-col text- justify-center h-screen items-center gap-4'>
+            <h1 className="text-[#5c1804] text-4xl font-bold">Theres a lot of beauty in creating moments</h1>
+            <p className="text-white">im a self taught photographer documenting spaces and people</p>
+                <h2 className="text-white text-2xl">learn more about me</h2>
+        </div>
+    )
 }
 export default Hero
